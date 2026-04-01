c4e548f2841a5c5f3c88336787072d1ce351fea0
Hugo Bel <hugobel.nqn@gmail.com>
2026-04-01 16:07:38 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Route::get       | Consultar
+ * Route::post      | Guardar
+ * Route::delete    | Eliminar
+ * Route::put       | Actualizar
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta Home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function($slug) {
+    //consulta a base de datos
+
+    return $slug;
+});
+
+Route::get('buscar', function(Request $request) {
+    //consulta a base de datos
+    
+    return $request->all();
 });
