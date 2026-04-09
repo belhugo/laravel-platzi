f304aa91dde5f5fc303a706ff512952cd3d0cb3f
Hugo Bel <hugobel.nqn@gmail.com>
2026-04-09 15:30:10 -0300

    Models

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence;
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'body' => $this->faker->text(2200)
+        ];
+
+    }
+}
