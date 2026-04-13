cf2c7a0ffcb07f9e2aa61c9f8f9600016005a06f
Hugo Bel <hugobel.nqn@gmail.com>
2026-04-13 19:14:00 -0300

    Delete Post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,8 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Listado de publicaciones
-
+                    
                     <table class="mb-4">
                         @foreach($posts as $post)
                         <tr class="border-b border-gray-200 text-sm">
@@ -19,7 +18,17 @@
                                 <a href="#" class="text-indigo-600">Editar</a>
                             </td>
                             <td class="px-6 py-4">
-                            	Eliminar
+                            	<form action="{{ route('posts.destroy', $post) }}" method="POST">
+								    @csrf 
+								    @method('DELETE')
+
+								    <input 
+								    	type="submit" 
+								    	value="Eliminar" 
+								    	class=" border border-red-600 text-red-600 rounded px-4 py-2" 
+								    	onclick="return confirm('¿Desea Eliminar?')"
+								    >
+								</form>
                             </td>
                         </tr>
                         @endforeach
